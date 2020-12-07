fc09d43e41d63a5afcdbf9fe542df4b65dc780e8
Bohdan-Antymys <73041493+Bohdan-Antymys@users.noreply.github.com>
2020-12-07 12:15:02 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+
+main() {
+    float x;
+    
+    printf ("Enter the value of x = ");
+    scanf ("%f", &x);
+    
+    float fx = atan (x);
+    printf ("f(x) = arctg(x) = %f\n", fx);
+    
+    main(&x)
+    printf ("f`(x) = (1 / (x^2) + 1) = %f\n", f1x)
+    
+}
+
+int main1(int *x1) {
+    
+    float f1x = (1 / (pow(*x1, 2) + 1));
+    printf ("f`(x) = (1 / (x^2) + 1) = %f\n", f1x)
+    
+}
